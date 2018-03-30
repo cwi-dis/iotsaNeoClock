49d586fcd7c88781fc163c8ff1f382d52947b03b
Jack Jansen <Jack.Jansen@cwi.nl>
2018-03-31 01:02:00 +0200

    Fixed Esp.h pathname

--- iotsaNeoClock.ino
+++ iotsaNeoClock.ino
@@ -9,7 +9,7 @@
 // Look for the string CHANGE to see where you need to add things for your application.
 //
 
-#include <ESP.h>
+#include <Esp.h>
 #include <FS.h>
 #include "iotsa.h"
 #include "iotsaWifi.h"
